824535df2d0e3b44b49429e7e1b1c71f3f876963
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-04-08 18:08:22 -0300

    faz o 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -44,6 +44,69 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    title = selector.css("h1.tec--article__header__title::text").get()
+
+    share_count_raw = selector.css(
+        "nav.tec--toolbar div.tec--toolbar__item::text"
+        ).get()
+
+    share_count_string = "0"
+    if share_count_raw is not None:
+        share_count_string = share_count_raw.split(sep=" ")[1]
+
+    share_count = int(share_count_string)
+
+    coments_count_string = selector.css(
+        "#js-comments-btn::text"
+        ).getall()[1].split(sep=" ")[1]
+    coments_count = int(coments_count_string)
+
+    categories = [
+        category[1:len(category)-1]
+        for category in selector.css(
+            "#js-categories a.tec--badge::text").getall()
+        ]
+
+    sources = [
+        source[1:len(source)-1]
+        for source in selector.css("div.z--mb-16 a.tec--badge::text").getall()
+      ]
+
+    writer_raw = selector.css(
+        ".z--font-bold *::text"
+        ).get()
+
+    writer = None
+
+    if writer_raw is not None:
+        writer = writer_raw
+        if writer_raw[0] == " " and writer_raw[-1] == " ":
+            writer = writer_raw[1:-1]
+
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+
+    summary = ''.join(
+        selector.css(
+            "div.tec--article__body p:nth-child(1) *::text"
+        ).getall()
+    )
+
+    url = selector.css("head link:nth-child(26)::attr(href)").get()
+
+    dictToReturn = {
+        "title": title,
+        "shares_count": share_count,
+        "comments_count": coments_count,
+        "categories": categories,
+        "sources": sources,
+        "writer": writer,
+        "timestamp": timestamp,
+        "summary": summary,
+        "url": url
+    }
+    return dictToReturn
 
 
 # Requisito 5
