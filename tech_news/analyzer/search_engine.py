6bdb88a90633e2159593b3448737a773c6062c33
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-04-11 18:10:34 -0300

    faz o 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from ..database import find_news
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    all_news = find_news()
+    print(all_news)
+    filtered_news = [
+        (new["title"], new["url"])
+        for new in all_news if title.lower() in new["title"].lower()
+    ]
+    return filtered_news
 
 
 # Requisito 7
